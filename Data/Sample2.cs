bd3320bcc1a90345272be710ecae5f2d264f3fe4
aki017 <aki@aki017.info>
2016-11-25 18:27:13 +0900

    initial commit

---
+++ Data/Sample2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using ZeroFormatter;
+
+namespace ConsoleApplication
+{
+  namespace Data
+  {
+    [ZeroFormattable]
+    public class Sample2
+    {
+      [Index(0)]  public virtual string Message { get; set; }
+      [Index(1)]  public virtual Sample2Child Child { get; set; }
+
+
+      public void Sample() {
+        Message = "Sample Message";
+        Child = new Sample2Child(){
+          Message = "Sample Child Message"
+        };
+      }
+    }
+
+    [ZeroFormattable]
+    public class Sample2Child
+    {
+      [Index(0)]  public virtual string Message { get; set; }
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using ZeroFormatter;
+
+namespace ConsoleApplication
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var targets = new []{ 
+        typeof(Data.Sample1),
+        typeof(Data.Sample2),
+      };
+      foreach(var t in targets) {
+        MethodInfo method = typeof(Program).GetMethod("Test", BindingFlags.Static | BindingFlags.Public );
+        method.MakeGenericMethod(t).Invoke(null, null);
+      }
+    }
+
+    public static void Test<T>() where T : new(){
+      Console.WriteLine(typeof(T).Name);
+      dynamic obj = new T();
+      obj.Sample();
+      var data = ZeroFormatterSerializer.Serialize<T>(obj);
+      File.WriteAllBytes($"packed/{typeof(T).Name}.pack", data);
+    }
+  }
+}
