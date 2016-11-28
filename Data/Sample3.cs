f64cf966d03004a830224cce5f49c5f4107b1a91
aki017 <aki@aki017.info>
2016-11-28 19:46:22 +0900

    Sample3

---
+++ Data/Sample3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using ZeroFormatter;
+
+namespace ConsoleApplication
+{
+  namespace Data
+  {
+    [ZeroFormattable]
+    public class Sample3
+    {
+      [Index(0)] public virtual string[]      MessageArray        { get; set; }
+      [Index(1)] public virtual List<string>  MessageListGeneric  { get; set; }
+      [Index(2)] public virtual IList<string> MessageIListGeneric { get; set; }
+
+      public void Sample() {
+        MessageArray        = new []{ "Hello", "Array" };
+        MessageListGeneric  = new List<string>{ "Hello", "List",  "Generic" };
+        MessageIListGeneric = new List<string>{ "Hello", "IList", "Generic" };
+      }
+    }
+  }
+}
